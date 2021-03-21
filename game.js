aa6ecd3ab4719183ba615184d2f39fb274de489c
martamazurkiewicz <marta@mazurkiewi.cz>
2021-03-21 19:21:13 +0100

    2 player version works

--- game.js
+++ game.js
@@ -1,21 +1,69 @@
-var playersMark = null;
+var playerMark = null;
 var flagedTiles = null;
 var playersMove = true;
+var mode = null;
+
 function startGame() {
-    playersMark = getPlayersMark();
-    document.getElementById("markDiv").remove();
+    console.log(mode);
+    console.log(playerMark);
+    var gameDiv = document.getElementById('innerGameDiv');
+    gameDiv.innerHTML = '';
     drawGame();
     clearFlagedTiles();
 }
+
+function setMode() {
+    document.getElementById('playGameButton').remove();
+    var gameDiv = document.getElementById('innerGameDiv');
+    var gameTextNode = document.getElementById('gameTextNode');
+    gameTextNode.innerHTML = 'Select mode:'
+    gameDiv.appendChild(createButton('Single Player', singlePlayerMode));
+    gameDiv.appendChild(createButton('Two Players', twoPlayersMode));
+}
+function twoPlayersMode() {
+    playerMark = 'cross';
+    mode = 2;
+    startGame();
+}
+function singlePlayerMode() {
+    mode = 1;
+    setMark()
+}
+function createButton(innerHTML, onClickFunc, args = null) {
+    var button = document.createElement('button');
+    button.type = 'button';
+    button.innerHTML = innerHTML;
+    button.addEventListener('click', function () {
+        if (args == null) {
+            onClickFunc();
+        }
+        else {
+            onClickFunc(args);
+        }
+    });
+    //button.onClick = onClickFunc();
+    return button;
+}
+
+function setMark() {
+    var gameDiv = document.getElementById('innerGameDiv');
+    gameDiv.innerHTML = '';
+    var gameTextNode = document.getElementById('gameTextNode');
+    gameTextNode.innerHTML = 'Select your mark:'
+    gameDiv.appendChild(createButton('Cross', setMarkOnClick, 'cross'));
+    gameDiv.appendChild(createButton('Circle', setMarkOnClick, 'circle'));
+}
+
+function setMarkOnClick(value) {
+    playerMark = value;
+    startGame();
+}
+
+
 function drawGame() {
     var board = document.getElementById("boardTable");
-    var span = document.createElement('span');
-    span.setAttribute('class', 'move');
-    var textNode = document.createTextNode('Your move!');
-    textNode.id = 'moveTextNode';
-    span.appendChild(textNode);
+    document.getElementById('gameTextNode').innerHTML = playerMark + ' move!'
     var gameDiv = document.getElementById("gameDiv");
-    gameDiv.insertBefore(span, board);
     drawBoard(board);
     gameDiv.appendChild(createPlayAgainButton());
 }
@@ -34,67 +82,75 @@ function createTile(tileNumber) {
     var tile = document.createElement('button');
     tile.id = tileNumber;
     tile.type = 'button';
-    tile.innerHTML = ' ';
     tile.className = 'tile';
     tile.addEventListener('click', function () {
-        drawMark(tile.id);
+        playerMove(tile.id);
     });
     return tile;
 }
 function createPlayAgainButton() {
     var button = document.createElement('button');
-    button.id = 'playAgainButton';
+    button.id = 'playGameButton';
     button.type = 'button';
     button.innerHTML = 'PLAY AGAIN >';
     button.className = 'playAgain';
-    button.onClick = playAgain();
+    button.onclick = playAgain;
     return button;
 }
-function drawMark(tileId) {
+function playerMove(tileId) {
     var tile = document.getElementById(tileId);
-    playerMove(tile);
-    flagedTiles[tileId[0]][tileId[1]] = playersMark;
-    if(!checkIfGameIsWon(tileId, playersMark) && !checkIfGameIsFinished()){
-        computerMove();
+    console.log(playerMark);
+    drawTile(tile);
+    if (isGameWon(tileId)) {
+        gameFinished();
     }
-}
-function getPlayersMark() {
-    var marks = document.getElementsByName('marks');
-    for (var i = 0; i < marks.length; i++) {
-        if (marks[i].checked) {
-            return marks[i].value;
+    else if (isGameFinished()) {
+        gameFinishedDraw();
+    }
+    else {
+        if (mode == 1) {
+            computerMove();
+        }
+        else {
+            if (playerMark == 'cross') {
+                playerMark = 'circle';
+            }
+            else {
+                playerMark = 'cross';
+            }
         }
     }
 }
-function checkIfGameIsFinished(){
-    for(var i = 0; i < 3; i++){
-        for(var j = 0; j < 3; j++){
-            if(flagedTiles[i][j] == null){
+
+function isGameFinished() {
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (flagedTiles[i][j] == null) {
                 return false;
-            }  
+            }
         }
     }
     return true;
 }
-function checkIfGameIsWon(tileId, mark) {
-    for(var i = 0; i < 3; i++){
-        if(flagedTiles[tileId[0]][i] != mark){
+function isGameWon(tileId) {
+    for (var i = 0; i < 3; i++) {
+        if (flagedTiles[tileId[0]][i] != playerMark) {
             break;
         }
-        if(i == 2){
+        if (i == 2) {
             return true;
         }
     }
-    for(var i = 0; i < 3; i++){
-        if(flagedTiles[i][tileId[1]] != mark){
+    for (var i = 0; i < 3; i++) {
+        if (flagedTiles[i][tileId[1]] != playerMark) {
             break;
         }
-        if(i == 2){
+        if (i == 2) {
             return true;
         }
     }
-    if(flagedTiles[1][1] == mark){
-        if((flagedTiles[0][0]==mark && flagedTiles[2][2]==mark) || (flagedTiles[0][2]==mark && flagedTiles[2][0]==mark)){
+    if (flagedTiles[1][1] == playerMark) {
+        if ((flagedTiles[0][0] == playerMark && flagedTiles[2][2] == playerMark) || (flagedTiles[0][2] == playerMark && flagedTiles[2][0] == playerMark)) {
             return true;
         }
     }
@@ -102,68 +158,101 @@ function checkIfGameIsWon(tileId, mark) {
 }
 function clearFlagedTiles() {
     flagedTiles = new Array(3);
-    for(var i = 0; i < 3; i++){
+    for (var i = 0; i < 3; i++) {
         flagedTiles[i] = new Array(3);
-        for(var j = 0; j < 3; j++){
+        for (var j = 0; j < 3; j++) {
             flagedTiles[i][j] = null;
         }
     }
 }
 function clearBoard() {
-    for(var i = 0; i < 3; i++){
-        for(var j = 0; j < 3; j++){
-            var tileId = String(i) + String(j);
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            var tileId = String(i)+String(j);
             var tile = document.getElementById(tileId);
-            tile.style.backgroundImage = 'url(blank.png)';
+            tile.style.background = 'white';
             tile.disabled = false;
         }
     }
 }
 function playAgain() {
-    clearFlagedTiles();
-    clearBoard();
-    playersMove = true;
+    document.getElementById("gameTextNode").innerHTML = "";
+    document.getElementById("boardTable").innerHTML = "";
+    setMode();
 }
-function getWinner() {
+function gameFinished(){
+    alert(playerMark +' won!');
+    disableAllTiles();
+    //createPlayGameButton();
+}
+// function createPlayGameButton(){
+    
+//     var button = document.createElement('button');
+//     button.type = 'button';
+//     button.id = 'playGameButton';
+//     button.innerHTML = 'Play Tic-tac-toe!';
+//     button.onClick = "setMode()";
+//     var headlinedInnerGameDiv = document.getElementById('headlinedInnerGameDiv');
+//     headlinedInnerGameDiv.appendChild(button);
+// }
+function gameFinishedDraw() {
+    alert('No one won!');
+    disableAllTiles();
+    //createPlayGameButton();
 
 }
-function displayWinner() {
-
-}
-function drawCross(tile) {
-    tile.style.backgroundImage = 'url(cross.png)';
-    tile.disabled = true;
-}
-function drawCircle(tile) {
-    tile.style.backgroundImage = 'url(circle.png)';
-    tile.disabled = true;
-}
-function playerMove(tile) {
-    if(playersMark == "cross"){
-        drawCross(tile);
-    }
-    else{
-        drawCircle(tile);
-    }
-}
-function computerMove() {
-    var availableIndexes = new Array();
-    for(var i = 0; i < 3; i++){
-        for(var j = 0; j < 3; j++){
-            if(flagedTiles[i][j] == null)
-            {
-                availableIndexes.push(String(i)+String(j));
+function disableAllTiles(){
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            var tile = document.getElementById(String(i) + String(j))
+            tile.disabled = true;
+            if(flagedTiles[i][j] == null){
+                tile.style.backgroundImage = 'url(blank.png)';
             }
         }
     }
-    var tileId = availableIndexes[Math.floor(Math.random() * (availableIndexes.length+1))];
-    var tile = document.getElementById(tileId);
-    if(playersMark != "cross"){
-        drawCross(tile);
-        flagedTiles[tileId[0]][tileId[1]] = "cross";
+}
+
+function changeTileBackGround(tile) {
+    if (playerMark == 'cross') {
+        tile.style.backgroundImage = 'url(cross.png)';
     }
-    else{
-        drawCircle(tile);
+    else {
+        tile.style.backgroundImage = 'url(circle.png)';
+    }
+    tile.disabled = true;
+}
+
+function drawTile(tile) {
+    changeTileBackGround(tile);
+    flagedTiles[tile.id[0]][tile.id[1]] = playerMark;
+}
+function computerMove() {
+    var availableIndexes = new Array();
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (flagedTiles[i][j] == null) {
+                availableIndexes.push(String(i) + String(j));
+            }
+        }
+    }
+    var tileId = availableIndexes[Math.floor(Math.random() * (availableIndexes.length + 1))];
+    var tile = document.getElementById(tileId);
+    if (playerMark != "cross") {
+        changeTileBackGround(tile, "cross");
+        flagedTiles[tileId[0]][tileId[1]] = "cross";
+        if (isGameWon(tileId, "cross")) {
+            gameFinished("computer");
+        }
+    }
+    else {
+        changeTileBackGround(tile, "circle");
         flagedTiles[tileId[0]][tileId[1]] = "circle";
+        if (isGameWon(tileId, "circle")) {
+            gameFinished("computer");
+        }
+    }
+    if (isGameFinished()) {
+        gameFinishedDraw();
     }
 }
